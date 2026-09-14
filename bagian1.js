0ef1f47dc6840ada3483d702be9531feb189306e
yuzhuruuu <annisayusri59@gmail.com>
2026-09-14 20:33:24 +0700

    finish

--- bagian1.js
+++
@@ -1,7 +0,0 @@
-function calculateDiscountedPrice(price, discountPercent) {
-    return price - (price * discountPercent) / 100;
-}
-
-const finalPrice = calculateDiscountedPrice(1000, 20);
-
-console.log("Harga setelah diskon:", finalPrice);
